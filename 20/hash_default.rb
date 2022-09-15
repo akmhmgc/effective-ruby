d81a33e10c45cf268e86163128eb6bdb8d802be3
akmhmgc <akimuhamaguchi@icloud.com>
2022-09-15 23:35:39 +0900

    209

--- 20/hash_default.rb
+++ 20/hash_default.rb
@@ -1,10 +1,9 @@
-
 hash1 = Hash.new([])
 # 存在しないキーを与えた時に常に同じハッシュを参照する
 hash1[:hoge].equal?(hash1[:fuga]) #=> true
 
 # 存在しないキーを入れる度にに毎回ブロックが評価される
-hash2 = Hash.new{[]}
+hash2 = Hash.new { [] }
 hash2[:hoge].equal?(hash2[:fuga]) #=> false
 
 # 評価されたブロックに対して << 'hogeしているだけ
@@ -13,3 +12,10 @@ hash2[:hoge] #=> []
 
 hash2[:hoge] = hash2[:hoge] << 'hoge'
 hash2[:hoge] #=> ['hoge']
+
+hash3 = Hash.new { |hash, key| hash[key] = [] }
+
+hash3[:hoge] << 'hoge'
+p hash3.keys
+p hash3[:hoge]
+p hash3[:fuga]
